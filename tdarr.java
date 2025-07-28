8a56dd0783b7b32239cc5a0e7e07836d685f134a
pritibhondwe <bhondwep88@gmail.com>
2025-07-28 13:52:27 +0530

    Create tdarr.java

---
+++ tdarr.java
@@ -0,0 +1,16 @@
+public class tdarr
+ {
+    public static void main(String[] args) 
+    {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        for(int x[]:arr)
+        {
+            for(int a:x)
+            {
+                System.out.print(a +" ");
+            }
+            System.out.println();
+        }
+    }
+}
+ 
