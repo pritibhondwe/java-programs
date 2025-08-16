d7697b2e4b62372aae3ee08cca7bffb31594de7c
pritibhondwe <bhondwep88@gmail.com>
2025-08-16 11:03:45 +0530

    Create DuplicateChars.java

---
+++ DuplicateChars.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DuplicateChars {
+    public static void main(String[] args) {
+        String str = "programming";
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+        }
+    }
+}
