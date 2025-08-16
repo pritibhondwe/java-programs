ffffe99123e973351d5995f55ba7db039117e1b9
pritibhondwe <bhondwep88@gmail.com>
2025-08-16 11:06:01 +0530

    Create LargestInArray.java

---
+++ LargestInArray.java
@@ -0,0 +1,13 @@
+public class LargestInArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 89, 34, 23};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest: " + max);
+    }
+}
